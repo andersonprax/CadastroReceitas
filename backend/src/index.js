bc8388d6579aaaab2ad38d469298eda3dbd31cb1
andersonprax <andersonintendope@gmail.com>
2020-07-30 19:47:39 -0300

    alteração na porta do servidor no arquivo index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -164,6 +164,6 @@ app.delete("/ingrediente/:id", (req,res) => {
 /**
  * Server
  */
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
 });
